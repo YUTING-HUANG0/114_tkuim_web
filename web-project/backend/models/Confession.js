72b32c49c10b24eacf0e98f04c194f0a22dfcbc2
DT2005 <ab919055@gmail.com>
2025-12-24 17:25:43 +0800

    chore: update project

---
+++ web-project/backend/models/Confession.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const confessionSchema = new mongoose.Schema({
+    content: {
+        type: String,
+        required: true,
+        maxlength: 500
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    views: {
+        type: Number,
+        default: 0
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Confession', confessionSchema);
